dae6ccd5fdc604e8d73f1a4b8d1ba8187efcfd9f
salatsynskahv <salatsynskahv@gmail.com>
2023-09-29 15:10:56 +0300

    dashboard4

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,46 +1,39 @@
 "use client"
 
-// import {useState} from "react";
-//
-// import {shallow} from "zustand/shallow";
-// import {useRouter} from "next/navigation";
-// import Tabs from "../../components/Tabs";
-// import {useAppStore} from "../../store/slice";
-// import CategoryList from "../../components/categoryDashboard/CategoryList";
-// import AllList from "../../components/AllList";
-// import Statistics from "../../components/chart/Statistics";
+import React, {useState} from "react";
+
+import {shallow} from "zustand/shallow";
+import {useAppStore} from "../../store/slice";
+import CategoryList from "../../components/categoryDashboard/CategoryList";
+import AllList from "../../components/AllList";
+import Statistics from "../../components/chart/Statistics";
+import Tabs from "@/components/Tabs";
 
 export default function Page() {
-    // const [tabState, setTabState] = useState([1, 0, 0]);
-    // const [allExpences, username] = useAppStore(
-    //     (state) => [state.allExpences, state.username],
-    //     shallow
-    // );
-    // const router = useRouter();
-    //
-    // if(!allExpences || allExpences.length <= 0) {
-    //     router.push('/');
-    // }
-    //
-    // function getTargetComponent() {
-    //     return (
-    //         tabState[0] && <CategoryList allRecords={allExpences}/> ||
-    //         tabState[1] && <AllList allExpences={allExpences}/> ||
-    //         tabState[2] && <Statistics/>
-    //     )
-    // }
+    const [tabState, setTabState] = useState([1, 0, 0]);
+    const [allExpences, username] = useAppStore(
+        (state) => [state.allExpences, state.username],
+        shallow
+    );
+    function getTargetComponent() {
+        return (
+            tabState[0] && <CategoryList allRecords={allExpences}/> ||
+            tabState[1] && <AllList allExpences={allExpences}/> ||
+            tabState[2] && <Statistics/>
+        )
+    }
 
     return (
         <div className="tabs-main">
             {/*{username}*/}
             Hello
-            {/*<div className="center-container">*/}
-            {/*    <Tabs tabState={tabState} setTabState={setTabState}/>*/}
-            {/*</div>*/}
-            {/*<div className="tabs-container">*/}
-            {/*    {*/}
-            {/*        getTargetComponent()*/}
-            {/*    }*/}
-            {/*</div>*/}
+            <div className="center-container">
+                <Tabs tabState={tabState} setTabState={setTabState}/>
+            </div>
+            <div className="tabs-container">
+                {
+                    getTargetComponent()
+                }
+            </div>
         </div>);
 }
